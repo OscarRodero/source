86dfa43950e5b2a323bd0086a9b2b5aafe47b5cc
Óscar <oscarrodero@hotmail.com>
2023-09-13 12:39:28 +0200

    Inicio Ejercicio 1 en clase

---
+++ repos/Tema1_Ejer1/Tema1_Ejer1/ManejarFechas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_Ejer1
+{
+    public class ManejarFechas
+    {
+        
+    }
+}
---
+++ repos/Tema1_Ejer1/Tema1_Ejer1/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Tema1_Ejer1
+{
+    using System;
+    internal class Program
+    {
+        private static string x;
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("¿Qué quieres hacer?");
+            Console.WriteLine();
+            Console.WriteLine("1.- Manejar Fechas");
+            Console.WriteLine("2.- Gestión Juegos");
+            Console.WriteLine("3.- Alquiler Puertos");
+            Console.WriteLine("4.- Salir");
+            
+            x = Console.ReadLine();
+            switch (x) {
+                case "1":
+                    ManejarFechas.Menu();
+                    break;
+                case "2":
+                    GestionJuegos();
+                    break;
+                case "3":
+                    AlquilerPuertos();
+                    break;
+                case "4":
+                    Salir();
+                    break;
+            }
+            
+        }
+
+        private static void Salir()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AlquilerPuertos()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GestionJuegos()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ManejarFechas()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DiaSemana()
+        {
+            Console.WriteLine("Introduce una fecha");
+            try
+            {
+                x = Console.ReadLine();
+                DateTime dateTime = DateTime.Parse(x);
+                Console.WriteLine("Fecha Introducida: " + dateTime);
+                Console.WriteLine("Dia: " + dateTime.DayOfWeek.ToString());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Unable to parse the specified date");
+            }
+            Console.WriteLine(x);
+        }
+
+    }
+}
