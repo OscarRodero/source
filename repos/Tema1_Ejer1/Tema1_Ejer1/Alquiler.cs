5d1aeeebef3be6f6cba77268194b138b49ab45b2
Óscar <oscarrodero@hotmail.com>
2023-09-19 13:37:32 +0200

    Ejer 3

---
+++ repos/Tema1_Ejer1/Tema1_Ejer1/Alquiler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_Ejer1
+{
+    public class Alquiler
+    {
+		private String nombre;
+		private String dni;
+		private DateTime fechaIni;
+		private DateTime fechaFin;
+		private int posicion;
+		private Barco embarcacion;
+
+		public Barco Embarcacion
+		{
+			get { return embarcacion; }
+			set { embarcacion = value; }
+		}
+
+		public int Posicion
+		{
+			get { return posicion; }
+			set { posicion = value; }
+		}
+
+		public DateTime FechaFin
+		{
+			get { return fechaFin; }
+			set { fechaFin = value; }
+		}
+
+
+		public DateTime FechaIni
+		{
+			get { return fechaIni; }
+			set { fechaIni = value; }
+		}
+
+		public String DNI
+		{
+			get { return dni; }
+			set { dni = value; }
+		}
+
+
+		public String Nombre
+		{
+			get { return nombre; }
+			set { nombre = value; }
+		}
+
+	}
+}
---
+++ repos/Tema1_Ejer1/Tema1_Ejer1/Barco.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_Ejer1
+{
+    public class Barco
+    {
+		public enum barquino { 
+		
+		}
+		private barquino tipoBarco;
+
+		public barquino TipoBarco
+		{
+			get { return tipoBarco; }
+			set { tipoBarco = value; }
+		}
+
+
+		private int metrosEslora;
+
+		public int MetrosEslora
+		{
+			get { return metrosEslora; }
+			set { metrosEslora = value; }
+		}
+
+	}
+}
